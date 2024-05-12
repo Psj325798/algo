e1b56ed15bd3d4a7df6fa25296da6f895a5f0ce2
Psj325798 <165372939+Psj325798@users.noreply.github.com>
2024-05-12 18:08:07 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 2524 ms, Memory: 155560 KB -BaekjoonHub

--- 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.py
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.py
@@ -1,26 +1,29 @@
+import sys
+
+input = sys.stdin.readline
+
+def binary_search(left, right, target):
+    while left <= right:
+        pivot = (left + right) // 2
+        if result[pivot] == target:
+            return pivot
+        elif result[pivot] < target:
+            left = pivot + 1
+        else:
+            right = pivot - 1
+
+    return left
+
 n = int(input())
-arr = list(map(int,input().split(" ")))
+arr = list(map(int, input().split()))
 
-def binary_search(start,end,target):
-    if start > end:
-        return start
-       
-    mid = (start+end) // 2
-    
-    if res[mid] > target:
-        return binary_search(start,mid-1,target)
-    elif res[mid] == target:
-        return mid
+result = [arr[0]]
+for i in range(1, n):
+    if result[-1] < arr[i]:
+        result.append(arr[i])
     else:
-        return binary_search(mid+1,end,target)
+        idx = binary_search(0, len(result), arr[i])
+        result[idx] = arr[i]
 
-res = [arr[0]]
+print(len(result))
 
-for i in range(1,len(arr)):
-    if res[-1] < arr[i]:
-        res.append(arr[i])
-    else:
-        res[binary_search(0,len(res)-1,arr[i])] = arr[i]
-
-
-print(len(res))
